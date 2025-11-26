55cd7702a446d3d2e96d6f3b6fdc16f89909bc77
mathurangy <123780033+mathurangy@users.noreply.github.com>
2025-11-25 20:14:30 -0500

    Update UserRepository.java

--- src/main/java/mytask/repository/UserRepository.java
+++ src/main/java/mytask/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package mytask.repository;
 
+
+
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,8 +9,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import mytask.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByUsername(String username);
+	Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
+    Optional<User> findByUsernameAndPassword(String username, String password);
 }
-
