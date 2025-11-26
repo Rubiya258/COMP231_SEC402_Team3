8547fcccc36deb462ca6e0692bc9ff8295fc47e5
mathurangy <123780033+mathurangy@users.noreply.github.com>
2025-11-25 20:43:53 -0500

    Update HomePage.jsx

--- frontendpage/src/pages/HomePage.jsx
+++ frontendpage/src/pages/HomePage.jsx
@@ -1,11 +1,15 @@
 import { useNavigate } from "react-router-dom";
 import "../styles/global.css";
+import logo from "../assets/logo.svg"; 
+import loginImage from "../assets/login.jpg";
 
 export default function HomePage() {
   const navigate = useNavigate();
 
   return (
     <div className="home-container">
+       <img src={logo} alt="App Logo" className="app-logo" />
+       <img src={loginImage} alt="Illustration" className="page-illustration" />
       <h1>Welcome to ReminderApp</h1>
       <div className="home-buttons">
         <button onClick={() => navigate("/login")}>Login</button>
