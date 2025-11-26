4c6909fc2f09a5035b12b38ac41a44e630dc5923
mathurangy <123780033+mathurangy@users.noreply.github.com>
2025-11-25 20:15:37 -0500

    Update NotificationValidator.java

--- src/main/java/mytask/service/NotificationValidator.java
+++ src/main/java/mytask/service/NotificationValidator.java
@@ -1,11 +1,9 @@
 package mytask.service;
 
 import java.time.LocalDateTime;
-
-import mytask.entity.Notification; 
+import mytask.entity.Notification;
 
 public class NotificationValidator {
-
 	public static void validate(Notification notification) {
         if (notification == null) {
             throw new IllegalArgumentException("Notification cannot be null.");
@@ -16,7 +14,7 @@ public class NotificationValidator {
         validateReminder(notification);
     }
 	
-	//Confirm notification time is in the future
+	
 	private static void validateNotificationTime(Notification notification) {
         LocalDateTime time = notification.getNotificationTime();
 
@@ -29,21 +27,21 @@ public class NotificationValidator {
         }
     }
 	
-	//Validate priority
+
 	 private static void validatePriority(Notification notification) {
 	        String priority = notification.getPriority();
 
 	        if (priority == null || priority.trim().isEmpty()) {
-	            // optional, can skip validation if not provided
+	          
 	            return;
 	        }
 
-	        if (!priority.matches("(?i)LOW|MEDIUM|HIGH")) {
+	        if (!priority.matches("LOW|MEDIUM|HIGH")) {
 	            throw new IllegalArgumentException("Priority must be LOW, MEDIUM, or HIGH (case-insensitive) if provided.");
 	        }
 	    }
 	 
-	 //Ensure notification is linked to reminder -- might delete later
+
 	 private static void validateReminder(Notification notification) {
 	        if (notification.getReminder() == null) {
 	            throw new IllegalArgumentException("Notification must be linked to a Reminder.");
