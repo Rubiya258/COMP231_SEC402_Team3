6d26b23722b351b3bb9a503a8476a078e5aabef9
mathurangy <123780033+mathurangy@users.noreply.github.com>
2025-11-25 20:06:52 -0500

    Update TaskAppBackendApplication.java

--- src/main/java/mytask/TaskAppBackendApplication.java
+++ src/main/java/mytask/TaskAppBackendApplication.java
@@ -2,14 +2,15 @@ package mytask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TaskAppBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskAppBackendApplication.class, args);
-	    System.out.println("Spring Boot MVC JPA app is ready ...");
-	
+		System.out.println("Spring Boot MVC JPA app is ready Practice test...");
 	}
 
 }
